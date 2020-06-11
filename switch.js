7cd1daf013dc18848877f8be4c3dd11ca984dfc1
Azroph <56892575+Azroph@users.noreply.github.com>
2020-06-11 19:34:02 +0200

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,19 @@
+let Antoine = {
+    nom: "Antoine",
+    age: 25,
+    sexe: "Masculin"
+}
+
+switch (Antoine.age) {
+      case 18 :
+          console.log ("Bravo tu as 18 ans")
+          break
+      case 25 :
+          console.log ("Bravo tu as 25 ans")
+      default : 
+          console.log ("Joyeux anniversaire")
+}
+
+if (Antoine.age = 33) {
+    console.log ("Tu n'as pas l'âge requis")
+}
